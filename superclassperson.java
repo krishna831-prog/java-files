b9a19de96e8ed582001c2c53d6bc46bee22d5992
krishna831-prog <77911299+krishna831-prog@users.noreply.github.com>
2023-07-12 22:23:13 +0530

    Create superclassperson.java

---
+++ superclassperson.java
@@ -0,0 +1,34 @@
+class Person {
+    private String name;
+    private int age;
+    private String address;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.setName("KRISHNA");
+        person.setAge(22);
+        person.setAddress("INDIA");
+        System.out.println("Name: " + person.getName());
+        System.out.println("Age: " + person.getAge());
+        System.out.println("Address: " + person.getAddress());
+    }
+}
